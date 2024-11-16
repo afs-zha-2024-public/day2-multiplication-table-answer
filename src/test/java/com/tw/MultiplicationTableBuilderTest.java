1f6d515817065b0bc3c8d2023aff35fd503f96ab
Jiancheng LI <jiancheng.li@thoughtworks.com>
2024-11-16 10:48:08 +0800

    [MultiplicationTable] test, should return false when given a number not between 1 and 1000

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -2,6 +2,7 @@ package com.tw;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MultiplicationTableBuilderTest {
@@ -18,5 +19,29 @@ class MultiplicationTableBuilderTest {
         assertTrue(isInRange);
     }
 
-    // TODO: add more tests for isInRange
+    @Test
+    void should_return_false_when_isInRange_given_a_number_0() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int number = 0;
+
+        // When
+        boolean isInRange = multiplicationTableBuilder.isInRange(number);
+
+        // Then
+        assertFalse(isInRange);
+    }
+
+    @Test
+    void should_return_false_when_isInRange_given_a_number_1001() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int number = 1001;
+
+        // When
+        boolean isInRange = multiplicationTableBuilder.isInRange(number);
+
+        // Then
+        assertFalse(isInRange);
+    }
 }
